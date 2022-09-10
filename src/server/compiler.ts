b0e212258b68b650cde1ed8eb3aa7886bf4241af
fcapolini <fabrizio.capolini@me.com>
2022-09-10 15:27:51 +0200

    compiler

--- src/server/compiler.ts
+++ src/server/compiler.ts
@@ -1,11 +1,27 @@
-import { StringBuf } from "../shared/util";
+import * as ESTree from "estree";
+import { generate } from "escodegen";
 import { HtmlDocument } from "./htmldom";
 
 export class Compiler {
 
-  static compile(doc: HtmlDocument, err?: CompilerError[]): string {
-    return '';
+  constructor(doc: HtmlDocument) {
+    this.docroot = docroot;
   }
+
+  compile(doc: HtmlDocument, err?: CompilerError[]): string {
+    const ret: ESTree.Program = {
+      type: 'Program',
+      sourceType: 'script',
+      body: [],
+    }
+    return generate(ret);
+  }
+
+  // ===========================================================================
+  // private
+  // ===========================================================================
+
+  _compileBody(doc:)
 
 }
 
--- test/server/compiler.test.ts
+++ test/server/compiler.test.ts
@@ -1,4 +1,5 @@
 import { assert } from "chai";
+import { parseScript } from "esprima";
 import { Compiler } from '../../src/server/compiler';
 import { HtmlDocument } from "../../src/server/htmldom";
 import Preprocessor from "../../src/server/preprocessor";
@@ -9,6 +10,10 @@ const preprocessor = new Preprocessor(process.cwd() + '/test/server/compiler');
 describe("compiler", () => {
 
   it("should compile minimal page", async () => {
+    const ast = parseScript(`(function () {
+    })()`);
+    console.log(ast);
+
     const doc = await preprocessor.reset().read('comp001.html');
     const code = Compiler.compile(doc as HtmlDocument);
     assert.equal(normalizeText(code), normalizeText(`(function() {
