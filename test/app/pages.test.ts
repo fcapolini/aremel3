c886d831894c0ae494c1e214bac876bf638188b6
fcapolini <fabrizio.capolini@me.com>
2022-10-11 18:17:56 +0200

    cleanup

--- test/app/pages.test.ts
+++ test/app/pages.test.ts
@@ -1,15 +1,14 @@
 import { assert } from "chai";
 import { generate } from "escodegen";
+import fs from "fs";
 import { compileApp } from "../../src/server/compiler";
+import { HtmlDocument } from "../../src/server/htmldom";
 import { load } from "../../src/server/loader";
 import Preprocessor from "../../src/server/preprocessor";
-import { DomDocument } from "../../src/shared/dom";
 import * as rt from "../../src/shared/runtime";
-import fs from "fs";
 import { normalizeSpace } from "../../src/shared/util";
-import { HtmlDocument } from "../../src/server/htmldom";
 
-const ROOTPATH = process.cwd() + '/test/server/pages';
+const ROOTPATH = process.cwd() + '/test/app/pages';
 const SUFFIX = '.in.html';
 const SUFFIX2 = '.out.html';
 const pages = fs.readdirSync(ROOTPATH)
@@ -20,26 +19,6 @@ const prepro = new Preprocessor(ROOTPATH);
 
 describe("pages", () => {
 
-  it('aaa', async () => {
-    var pre = new Preprocessor(ROOTPATH, [{
-      fname: 'index.html',
-      content: `<!DOCTYPE html>
-        <html>
-          <head></head>
-          <body></body>
-        </html>`
-    }]);
-    const app = await load('index.html', pre);
-    const doc = app.doc as DomDocument;
-    const ast = compileApp(app);
-    const src = generate(ast);
-    const obj = eval(`(${src})`);
-    const run = new rt.App(doc, obj);
-    assert.equal(run.state.cycle ?? 0, 0);
-    run.refresh();
-    assert.equal(run.state.cycle, 1);
-  });
-
   pages.forEach(p => {
     it(p, async () => {
       const app = await load(p + SUFFIX, prepro);
@@ -48,7 +27,9 @@ describe("pages", () => {
       const src = generate(ast);
       const obj = eval(`(${src})`);
       const run = new rt.App(doc, obj);
+      assert.equal(run.state.cycle ?? 0, 0);
       run.refresh();
+      assert.equal(run.state.cycle, 1);
       const txt = await fs.promises.readFile(
         `${ROOTPATH}/${p}${SUFFIX2}`,
         { encoding: 'utf8' }
