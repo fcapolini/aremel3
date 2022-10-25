f6528d98107e30428be14e718adf9300ae1cfab5
fcapolini <fabrizio.capolini@me.com>
2022-10-25 23:33:56 +0200

    server

--- test/server/server.test.ts
+++ test/server/server.test.ts
@@ -1,10 +1,18 @@
 import { assert } from "chai";
+import fs from "fs";
 import { JSDOM } from "jsdom";
-import { exec, execSync, ChildProcess } from "child_process";
 import Server from "../../src/server/server";
 import * as rt from "../../src/shared/runtime";
+import { normalizeSpace } from "../../src/shared/util";
 
-const ROOTPATH = process.cwd() + '/test/server/delivery';
+const ROOTPATH = process.cwd() + '/test/server/pages';
+const SOURCE_SUFFIX = '.source.html';
+const CACHED_SUFFIX = '.cached.html';
+const CHANGE_SUFFIX = '.change.html';
+const sources = fs.readdirSync(ROOTPATH)
+  .filter(v => v.endsWith(SOURCE_SUFFIX))
+  .map(v => v.substring(0, v.length - SOURCE_SUFFIX.length))
+  .sort();
 
 describe('server', function () {
   let server: Server;
@@ -19,16 +27,45 @@ describe('server', function () {
     server.stop();
   });
 
-  it('should deliver base.source.html', async () => {
-    const jsdom = await JSDOM.fromURL(
-      `http://localhost:${portNr}/base.source.html`, {
-        runScripts: "dangerously",
-        resources: "usable"
-      }
-    )
-    assert.exists(jsdom.window[rt.STATE_GLOBAL]);
-    const rtScript = jsdom.window.document.body.lastElementChild;
-    await new Promise<Event>(res => rtScript?.addEventListener('load', res));
-    assert.exists(jsdom.window[rt.APP_GLOBAL]);
+  sources.forEach(p => {
+    it(p, async () => {
+      const jsdom = await JSDOM.fromURL(
+        `http://localhost:${portNr}/${p}${SOURCE_SUFFIX}`, {
+          runScripts: "dangerously",
+          resources: "usable"
+        }
+      );
+      await new Promise<void>(resolve => {
+        const e = jsdom.window.document.querySelector('body > script:last-child');
+        e?.addEventListener('load', (_) => resolve());
+      });
+      const cachedPage = await fs.promises.readFile(
+        `${ROOTPATH}/${p}${CACHED_SUFFIX}`,
+        { encoding: 'utf8' }
+      );
+      assert.equal(
+        normalizeSpace(serialize(jsdom))?.trim(),
+        normalizeSpace(cachedPage)?.trim()
+      );
+      const app = jsdom.window[rt.APP_GLOBAL];
+      app.root.obj.change = 1;
+      const changePage = await fs.promises.readFile(
+        `${ROOTPATH}/${p}${CHANGE_SUFFIX}`,
+        { encoding: 'utf8' }
+      );
+      assert.equal(
+        normalizeSpace(serialize(jsdom))?.trim(),
+        normalizeSpace(changePage)?.trim()
+      );
+    });
   });
+
 });
+
+function serialize(jsdom: JSDOM): string {
+  return jsdom.serialize()
+  .replace(' async="" ', ' async ')
+  .replace('><html', '>\n<html')
+  .replace('><head', '>\n<head')
+  .replace('></html>', '>\n</html>');
+}
